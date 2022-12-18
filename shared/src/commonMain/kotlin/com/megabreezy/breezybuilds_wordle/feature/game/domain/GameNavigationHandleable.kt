6c0aedbed45cb05d04ba83cbaacce08a07dd35d3
breezybuildsagame <breezy@megabreezy.com>
2022-12-18 08:16:26 -0600

    BBW-36 Completed.

---
+++ shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/GameNavigationHandleable.kt
@@ -0,0 +1,8 @@
+package com.megabreezy.breezybuilds_wordle.feature.game.domain
+
+interface GameNavigationHandler
+{
+    fun onHelpOptionClicked()
+    fun onStatsOptionClicked()
+    fun onSettingsOptionClicked()
+}
