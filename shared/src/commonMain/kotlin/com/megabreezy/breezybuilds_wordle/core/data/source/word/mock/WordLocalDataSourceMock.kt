010641289383f0abf1102a71d006f24f1524699d
breezybuildsagame <breezy@megabreezy.com>
2022-12-29 07:59:50 -0600

    BBW-29 Completed.

--- shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/core/data/source/word/mock/WordLocalDataSourceMock.kt
+++ shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/core/data/source/word/mock/WordLocalDataSourceMock.kt
@@ -3,6 +3,8 @@ package com.megabreezy.breezybuilds_wordle.core.data.source.word.mock
 import com.megabreezy.breezybuilds_wordle.core.data.source.word.WordLocalDataManageable
 import com.megabreezy.breezybuilds_wordle.core.data.source.word.WordNotFoundLocalDataException
 import com.megabreezy.breezybuilds_wordle.core.domain.model.Word
+import com.megabreezy.breezybuilds_wordle.core.util.Scenario
+import org.koin.core.module.Module
 
 class WordLocalDataSourceMock(
     private val wordToReturn: Word? = null,
@@ -14,5 +16,24 @@ class WordLocalDataSourceMock(
         wordToReturn?.let { return it }
 
         throw WordNotFoundLocalDataException(message = getErrorMessage)
+    }
+
+    companion object
+    {
+        val mockWords = listOf(
+            "ALPHA", "FIRST", "PLACE", "SHAPE", "FLOAT"
+        )
+
+        fun injectDefinition(module: Module, scenarios: List<Scenario>)
+        {
+            val wordWasFound =  scenarios.firstOrNull { it == Scenario.WORD_FOUND } != null
+
+            module.single<WordLocalDataManageable>()
+            {
+                WordLocalDataSourceMock(
+                    wordToReturn = if (wordWasFound) Word(word = mockWords.random()) else null
+                )
+            }
+        }
     }
 }
