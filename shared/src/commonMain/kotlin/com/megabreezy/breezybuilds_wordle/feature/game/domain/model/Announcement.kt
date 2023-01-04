a54de53a1a385cfbe8455a3429ae52c04f26fd4c
breezybuildsagame <breezy@megabreezy.com>
2023-01-04 07:30:14 -0600

    BBW-18 update Announcement to enable setting of message.

--- shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/model/Announcement.kt
+++ shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/model/Announcement.kt
@@ -1,6 +1,8 @@
 package com.megabreezy.breezybuilds_wordle.feature.game.domain.model
 
-data class Announcement(private val message: String)
+data class Announcement(private var message: String? = null)
 {
     fun message() = this.message
+
+    fun setMessage(newMessage: String?) { this.message = newMessage }
 }
--- shared/src/commonTest/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/model/AnnouncementTests.kt
+++ shared/src/commonTest/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/model/AnnouncementTests.kt
@@ -2,6 +2,7 @@ package com.megabreezy.breezybuilds_wordle.feature.game.domain.model
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class AnnouncementTests
 {
@@ -16,5 +17,32 @@ class AnnouncementTests
 
         // then
         assertEquals(expectedMessage, sut.message())
+    }
+
+    @Test
+    fun `When setMessage method invoked with new message on instance - the message method returns the expected string value`()
+    {
+        // given
+        val expectedUpdatedMessage = "Another important announcement!"
+        val sut = Announcement()
+
+        // when
+        sut.setMessage(newMessage = expectedUpdatedMessage)
+
+        // then
+        assertEquals(expectedUpdatedMessage, sut.message())
+    }
+
+    @Test
+    fun `When setMessage method invoked with null value on instance - the message method returns expected null value`()
+    {
+        // given
+        val sut = Announcement(message = "My awesome message!")
+
+        // when
+        sut.setMessage(newMessage = null)
+
+        // then
+        assertNull(sut.message())
     }
 }
