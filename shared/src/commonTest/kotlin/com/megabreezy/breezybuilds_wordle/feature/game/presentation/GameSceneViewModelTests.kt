422205178c40513a7ab6926a8248e581ff5699e8
breezybuildsagame <breezy@megabreezy.com>
2023-01-07 07:45:40 -0600

    BBW-35 Completed.

--- shared/src/commonTest/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/presentation/GameSceneViewModelTests.kt
+++ shared/src/commonTest/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/presentation/GameSceneViewModelTests.kt
@@ -6,13 +6,18 @@ import com.megabreezy.breezybuilds_wordle.core.domain.model.Answer
 import com.megabreezy.breezybuilds_wordle.core.domain.model.Word
 import com.megabreezy.breezybuilds_wordle.core.util.CoreKoinModule
 import com.megabreezy.breezybuilds_wordle.core.util.Scenario
+import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.Announcement
+import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameBoard
+import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameKeyboard
 import com.megabreezy.breezybuilds_wordle.feature.game.util.GameKoinModule
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import kotlin.test.*
 
-class GameSceneViewModelTests
+class GameSceneViewModelTests: KoinComponent
 {
     private lateinit var answerLocalDataSource: MockAnswerLocalDataSource
 
@@ -52,40 +57,60 @@ class GameSceneViewModelTests
     fun `when getHeader method is invoked on an instance - the getHeader use case is invoked`()
     {
         // given
+        val expectedHeaderTitle = "WORDLE"
+        val sut = GameSceneViewModel()
 
         // when
+        val actualHeader = sut.getHeader()
 
         // then
+        assertEquals(expectedHeaderTitle, actualHeader.title())
     }
 
     @Test
     fun `when the getGameBoard method is invoked on an instance - the getGameBoard use case is invoked`()
     {
         // given
+        val sut = GameSceneViewModel()
+        val expectedGameBoard: GameBoard by inject()
+        sut.setUp()
 
         // when
+        val actualGameBoard = sut.getGameBoard()
 
         // then
+        assertEquals(expectedGameBoard, actualGameBoard)
     }
 
     @Test
     fun `when the getGameKeyboard method is invoked on an instance - the getGameKeyboard use case is invoked`()
     {
         // given
+        val sut = GameSceneViewModel()
+        val expectedKeyboard: GameKeyboard by inject()
+        var keyPressDidInvoke = false
+        expectedKeyboard.rows().first().first().setOnClick { keyPressDidInvoke = true }
 
         // when
+        sut.getGameKeyboard().rows().first().first().click()
 
         // then
+        assertTrue(keyPressDidInvoke)
     }
 
     @Test
     fun `when the getAnnouncement method is invoked on an instance - the getAnnouncement use case is invoked`()
     {
         // given
+        val expectedAnnouncement: Announcement by inject()
+        val sut = GameSceneViewModel()
+        expectedAnnouncement.setMessage(newMessage = "Testing a new announcement!")
 
         // when
+        val actualAnnouncement = sut.getAnnouncement()
 
         // then
+        assertEquals(expectedAnnouncement, actualAnnouncement)
     }
 
     class MockAnswerLocalDataSource: AnswerLocalDataManageable
