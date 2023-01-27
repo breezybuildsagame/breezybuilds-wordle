f8575cd87fcb1845c349d75d5724eba20b64f67d
breezybuildsagame <breezy@megabreezy.com>
2023-01-27 07:53:25 -0600

    BBW-59 board Tile Composable continued.

--- androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/game/presentation/component/GameSceneBoardTests.kt
+++ androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/game/presentation/component/GameSceneBoardTests.kt
@@ -1,15 +1,20 @@
 package com.megabreezy.breezybuilds_wordle.game.presentation.component
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.ui.test.assertHeightIsEqualTo
-import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
 import com.megabreezy.breezybuilds_wordle.android.game.presentation.component.GameSceneBoard
+import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
+import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 import com.megabreezy.breezybuilds_wordle.core.ui.SceneMock
+import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameBoard
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -56,5 +61,70 @@ class GameSceneBoardTests
         // then
         composeTestRule.onNodeWithContentDescription(GameSceneBoard.TagName.TILE.toString()).assertWidthIsEqualTo(expectedSize!!)
         composeTestRule.onNodeWithContentDescription(GameSceneBoard.TagName.TILE.toString()).assertHeightIsEqualTo(expectedSize!!)
+    }
+
+    @Test
+    fun when_hidden_tile_is_displayed__component_matches_design_requirements()
+    {
+        // given
+        lateinit var expectedBorder: BorderStroke
+
+        // when
+        composeTestRule.setContent()
+        {
+            SceneMock.display()
+            {
+                BoxWithConstraints()
+                {
+                    expectedBorder = BorderStroke(
+                        color = MaterialTheme.colors.error,
+                        width = this.maxHeight * (2 /Scene.idealFrame().height)
+                    )
+                }
+
+                GameSceneBoard.Tile.Component(
+                    options = GameSceneBoard.Tile.ComponentOptions(
+                        state = GameBoard.Tile.State.HIDDEN
+                    )
+                )
+            }
+        }
+
+        // then
+        composeTestRule.onNode(
+            SemanticsMatcher.expectValue(GameSceneBoard.Tile.BorderStrokeKey, expectedBorder)
+        ).assertExists()
+    }
+
+    @Test
+    fun when_tile_with_letter_is_displayed__component_matches_design_requirements()
+    {
+        // given
+        lateinit var expectedTextStyle: TextStyle
+
+        // when
+        composeTestRule.setContent()
+        {
+            SceneMock.display()
+            {
+                BoxWithConstraints()
+                {
+                    expectedTextStyle = TextStyle(
+                        color = MaterialTheme.colors.onPrimary,
+                        fontFamily = ThemeFonts.roboto,
+                        fontSize = dpToSp(dp = this.maxHeight * (35 / Scene.idealFrame().height)),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                GameSceneBoard.Tile.Component(options = GameSceneBoard.Tile.ComponentOptions(letter = "G"))
+            }
+        }
+
+        // then
+        composeTestRule.onNode(
+            SemanticsMatcher.expectValue(GameSceneBoard.Tile.TextStyleKey, expectedTextStyle)
+        ).assertExists()
+        composeTestRule.onNodeWithContentDescription(GameSceneBoard.TagName.TILE.toString()).onChild().assertTextEquals("G")
     }
 }
--- androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/game/presentation/component/GameSceneBoard.kt
+++ androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/game/presentation/component/GameSceneBoard.kt
@@ -1,29 +1,80 @@
 package com.megabreezy.breezybuilds_wordle.android.game.presentation.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
 import com.megabreezy.breezybuilds_wordle.android.core.util.LocalSceneDimensions
+import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
+import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
+import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameBoard
 
 object GameSceneBoard
 {
     object Tile
     {
+        val BorderStrokeKey = SemanticsPropertyKey<BorderStroke>("BorderStroke")
+        val TextStyleKey = SemanticsPropertyKey<TextStyle>("TextStyle")
+
+        private var SemanticsPropertyReceiver.gameSceneBoardTileBorderStroke by BorderStrokeKey
+        private var SemanticsPropertyReceiver.gameSceneBoardTileTextStyle by TextStyleKey
+
         @Composable
-        fun Component()
+        fun Component(options: ComponentOptions = ComponentOptions())
         {
+            val borderStroke = BorderStroke(
+                width = LocalSceneDimensions.current.height * (2 / Scene.idealFrame().height),
+                color = MaterialTheme.colors.error
+            )
+            val textStyle = TextStyle(
+                color = MaterialTheme.colors.onPrimary,
+                fontFamily = ThemeFonts.roboto,
+                fontSize = dpToSp(dp = LocalSceneDimensions.current.height * (35 / Scene.idealFrame().height)),
+                fontWeight = FontWeight.Bold
+            )
+
             Box(
+                contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .width(width = LocalSceneDimensions.current.width * (61 / Scene.idealFrame().width))
+                    .border(borderStroke)
                     .aspectRatio(ratio = 1f)
-                    .semantics { contentDescription = TagName.TILE.toString() }
+                    .semantics
+                    {
+                        contentDescription = TagName.TILE.toString()
+                        gameSceneBoardTileBorderStroke = borderStroke
+                    }
             )
+            {
+                if (options.letter.isNotEmpty())
+                {
+                    Text(
+                        text = options.letter,
+                        style = textStyle,
+                        modifier = Modifier
+                            .semantics { gameSceneBoardTileTextStyle = textStyle }
+                    )
+                }
+            }
         }
+
+        data class ComponentOptions(
+            val state: GameBoard.Tile.State = GameBoard.Tile.State.HIDDEN,
+            val letter: String = ""
+        )
     }
 
     enum class TagName(private val id: String)
