c88b0e803dbdf1b48ebb46a020134ea4d2ac461e
breezybuildsagame <breezy@megabreezy.com>
2023-02-06 07:34:10 -0600

    BBW-60 Completed.

--- androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/game/presentation/component/GameSceneKeyboardTests.kt
+++ androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/game/presentation/component/GameSceneKeyboardTests.kt
@@ -6,15 +6,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
 import com.megabreezy.breezybuilds_wordle.android.game.presentation.component.GameSceneKeyboard
+import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
+import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 import com.megabreezy.breezybuilds_wordle.core.ui.SceneMock
 import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameKeyboard
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -78,6 +83,7 @@ class GameSceneKeyboardTests
     fun when_composable_invoked_with_letters__expected_text_is_displayed()
     {
         // given
+        lateinit var expectedTextStyle: TextStyle
         val expectedLetter = "B"
 
         // when
@@ -85,6 +91,16 @@ class GameSceneKeyboardTests
         {
             SceneMock.display()
             {
+                BoxWithConstraints()
+                {
+                    expectedTextStyle = TextStyle(
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontFamily = ThemeFonts.roboto,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = dpToSp(dp = this.maxHeight * (13 / Scene.idealFrame().height))
+                    )
+                }
+
                 GameSceneKeyboard.Key.Component(
                     options = GameSceneKeyboard.Key.ComponentOptions(letters = expectedLetter)
                 )
@@ -95,6 +111,9 @@ class GameSceneKeyboardTests
         composeTestRule.onNodeWithContentDescription(
             GameSceneKeyboard.TagName.KEY.toString(), useUnmergedTree = true
         ).onChild().assertTextEquals(expectedLetter)
+        composeTestRule.onNode(
+            SemanticsMatcher.expectValue(GameSceneKeyboard.Key.ButtonTextStyle, expectedTextStyle)
+        ).assertExists()
     }
 
     @Test
@@ -253,5 +272,87 @@ class GameSceneKeyboardTests
         composeTestRule.onNode(
             SemanticsMatcher.expectValue(GameSceneKeyboard.Key.ButtonColorsKey, expectedButtonColors)
         ).assertExists()
+    }
+
+    @Test
+    fun when_correct_key_composable_invoked__color_matches_design_requirements()
+    {
+        // given
+        lateinit var expectedButtonColors: ButtonColors
+
+        // when
+        composeTestRule.setContent()
+        {
+            SceneMock.display()
+            {
+                expectedButtonColors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                )
+
+                GameSceneKeyboard.Key.Component(
+                    options = GameSceneKeyboard.Key.ComponentOptions(
+                        backgroundColor = GameKeyboard.Key.BackgroundColor.CORRECT
+                    )
+                )
+            }
+        }
+
+        // then
+        composeTestRule.onNode(
+            SemanticsMatcher.expectValue(GameSceneKeyboard.Key.ButtonColorsKey, expectedButtonColors)
+        ).assertExists()
+    }
+
+    @Test
+    fun when_key_composable_is_displayed_and_clicked__expected_function_is_invoked()
+    {
+        // given
+        var tappedDidInvoke = false
+
+        // when
+        composeTestRule.setContent()
+        {
+            GameSceneKeyboard.Key.Component(
+                options = GameSceneKeyboard.Key.ComponentOptions { tappedDidInvoke = true }
+            )
+        }
+        composeTestRule.onNodeWithContentDescription(GameSceneKeyboard.TagName.KEY.toString()).performClick()
+
+        // then
+        Assert.assertTrue(tappedDidInvoke)
+    }
+
+    @Test
+    fun when_composable_appears_with_rows__composable_matches_design_requirements()
+    {
+        // given
+        val expectedRowOne: List<@Composable () -> Unit> = listOf(
+                { GameSceneKeyboard.Key.Component(options = GameSceneKeyboard.Key.ComponentOptions(letters = "T")) },
+                { GameSceneKeyboard.Key.Component(options = GameSceneKeyboard.Key.ComponentOptions(letters = "E")) }
+        )
+        val expectedRowTwo: List<@Composable () -> Unit> = listOf(
+            { GameSceneKeyboard.Key.Component(options = GameSceneKeyboard.Key.ComponentOptions(letters = "S")) },
+            { GameSceneKeyboard.Key.Component(options = GameSceneKeyboard.Key.ComponentOptions(letters = "T")) }
+        )
+
+        // when
+        composeTestRule.setContent()
+        {
+            SceneMock.display()
+            {
+                GameSceneKeyboard.Component(
+                    options = GameSceneKeyboard.ComponentOptions(rows = listOf(expectedRowOne, expectedRowTwo))
+                )
+            }
+        }
+        composeTestRule.onRoot().printToLog("TAG")
+
+        // then
+        for (i in 0 until 4)
+        {
+            composeTestRule.onNodeWithContentDescription(
+                GameSceneKeyboard.TagName.ROWS.toString(), useUnmergedTree = true
+            ).onChildAt(index = i).onChild().assertTextEquals("TEST"[i].toString())
+        }
     }
 }
--- androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/game/presentation/component/GameSceneKeyboard.kt
+++ androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/game/presentation/component/GameSceneKeyboard.kt
@@ -1,10 +1,7 @@
 package com.megabreezy.breezybuilds_wordle.android.game.presentation.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -16,21 +13,55 @@ import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
 import com.megabreezy.breezybuilds_wordle.android.core.ui.image.ImageComponent
 import com.megabreezy.breezybuilds_wordle.android.core.util.LocalSceneDimensions
+import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
+import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameKeyboard
 
 object GameSceneKeyboard
 {
+    @Composable
+    fun Component(options: ComponentOptions = ComponentOptions())
+    {
+        Box()
+        {
+            Column(
+                modifier = Modifier
+                    .semantics { contentDescription = TagName.ROWS.toString() },
+                verticalArrangement = Arrangement.spacedBy(
+                    space = LocalSceneDimensions.current.height * (6 / Scene.idealFrame().height)
+                )
+            )
+            {
+                options.rows.forEach()
+                { row ->
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(
+                            LocalSceneDimensions.current.width * (4 / Scene.idealFrame().width)
+                        )
+                    )
+                    {
+                        row.forEach { key -> key() }
+                    }
+                }
+            }
+        }
+    }
+
     object Key:ImageComponent()
     {
         var ButtonShapeKey = SemanticsPropertyKey<RoundedCornerShape>("ButtonShape")
         var ButtonColorsKey = SemanticsPropertyKey<ButtonColors>("ButtonColors")
+        var ButtonTextStyle = SemanticsPropertyKey<TextStyle>("ButtonTextStyle")
 
         private var SemanticsPropertyReceiver.gameSceneKeyboardKeyButtonShape by ButtonShapeKey
         private var SemanticsPropertyReceiver.gameSceneKeyboardKeyButtonColors by ButtonColorsKey
+        private var SemanticsPropertyReceiver.gameSceneKeyboardKeyButtonTextStyle by ButtonTextStyle
 
         @Composable
         fun Component(options: ComponentOptions = ComponentOptions())
@@ -41,11 +72,18 @@ object GameSceneKeyboard
                     GameKeyboard.Key.BackgroundColor.DEFAULT -> MaterialTheme.colorScheme.background
                     GameKeyboard.Key.BackgroundColor.NOT_FOUND -> MaterialTheme.colorScheme.error
                     GameKeyboard.Key.BackgroundColor.NEARBY -> MaterialTheme.colorScheme.tertiary
+                    GameKeyboard.Key.BackgroundColor.CORRECT -> MaterialTheme.colorScheme.secondary
                     else -> Color.Transparent
                 }
             )
             val buttonShape = RoundedCornerShape(
                 corner = CornerSize(size = LocalSceneDimensions.current.height * (4 / Scene.idealFrame().height))
+            )
+            val buttonTextStyle = TextStyle(
+                color = MaterialTheme.colorScheme.onBackground,
+                fontFamily = ThemeFonts.roboto,
+                fontWeight = FontWeight.Bold,
+                fontSize = dpToSp(dp = LocalSceneDimensions.current.height * (13 / Scene.idealFrame().height))
             )
             val buttonWidth = if ((options.letters?.count() ?: 0) > 1 || options.resourceId != null) 52 else 33
 
@@ -63,13 +101,15 @@ object GameSceneKeyboard
                         gameSceneKeyboardKeyButtonColors = buttonColors
                     },
                 shape = buttonShape,
-                onClick = {  }
+                onClick = options.onClick
             )
             {
                 options.letters?.let()
                 {
                     Text(
-                        text = options.letters
+                        text = options.letters,
+                        modifier = Modifier.semantics { gameSceneKeyboardKeyButtonTextStyle = buttonTextStyle },
+                        style = buttonTextStyle
                     )
                 }
 
@@ -89,13 +129,16 @@ object GameSceneKeyboard
         data class ComponentOptions(
             val letters: String? = null,
             val resourceId: String? = null,
-            val backgroundColor: GameKeyboard.Key.BackgroundColor = GameKeyboard.Key.BackgroundColor.DEFAULT
+            val backgroundColor: GameKeyboard.Key.BackgroundColor = GameKeyboard.Key.BackgroundColor.DEFAULT,
+            val onClick: () -> Unit = { }
         )
     }
 
+    data class ComponentOptions(val rows: List<List<@Composable () -> Unit>> = listOf())
+
     enum class TagName(private val id: String)
     {
-        KEY(id = "game_scene_keyboard_key_component");
+        KEY(id = "game_scene_keyboard_key_component"), ROWS("game_scene_keyboard_rows_component");
 
         override fun toString() = this.id
     }
