aa6b207410ddf713fb312bd59f50c00ba5781dbd
breezybuildsagame <breezy@megabreezy.com>
2023-03-19 15:28:09 -0500

    Update Answer to add new playerGuessedCorrectly configurable property.

--- shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/core/domain/model/Answer.kt
+++ shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/core/domain/model/Answer.kt
@@ -2,14 +2,22 @@ package com.megabreezy.breezybuilds_wordle.core.domain.model
 
 data class Answer(
     private val word: Word,
-    private var isCurrent: Boolean = false
+    private var isCurrent: Boolean = false,
+    private var playerGuessedCorrectly: Boolean? = null
 )
 {
     fun isCurrent() = this.isCurrent
 
+    fun playerGuessedCorrectly(): Boolean? = this.playerGuessedCorrectly
+
     fun setIsCurrent(newIsCurrent: Boolean)
     {
         this.isCurrent = newIsCurrent
+    }
+
+    fun setPlayerGuessedCorrectly(newPlayerGuessedCorrectly: Boolean?)
+    {
+        this.playerGuessedCorrectly = newPlayerGuessedCorrectly
     }
 
     fun word(): Word = this.word
