dde49b77e7380b280a4511d9bff34ef4e7bae194
breezybuildsagame <breezy@megabreezy.com>
2023-04-07 06:44:12 -0500

    Refactor GetGameAnswer use case.

--- shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/use_case/GetGameAnswer.kt
+++ shared/src/commonMain/kotlin/com/megabreezy/breezybuilds_wordle/feature/game/domain/use_case/GetGameAnswer.kt
@@ -1,7 +1,6 @@
 package com.megabreezy.breezybuilds_wordle.feature.game.domain.use_case
 
 import com.megabreezy.breezybuilds_wordle.feature.game.domain.gateway.GameAnswerGateway
-import com.megabreezy.breezybuilds_wordle.feature.game.domain.gateway.GameAnswerNotCreatedRepositoryException
 import com.megabreezy.breezybuilds_wordle.feature.game.domain.gateway.GameAnswerNotFoundRepositoryException
 import com.megabreezy.breezybuilds_wordle.feature.game.domain.model.GameAnswer
 import org.koin.core.component.inject
@@ -9,22 +8,12 @@ import org.koin.core.component.inject
 suspend fun GameUseCase.getGameAnswer(attemptCreateOnFailure: Boolean = true): GameAnswer
 {
     val repository: GameAnswerGateway by inject()
+    var exception: Throwable?
 
-    return try
-    {
-        repository.get()
-    }
-    catch (e: GameAnswerNotFoundRepositoryException)
-    {
-        if (!attemptCreateOnFailure) throw GameUseCase.GetGameAnswerFailedException(message = e.message)
+    try { return repository.get() } catch (e: Throwable) { exception = e }
 
-        try
-        {
-            repository.create()
-        }
-        catch (e: GameAnswerNotCreatedRepositoryException)
-        {
-            throw GameUseCase.GetGameAnswerFailedException(message = e.message)
-        }
-    }
+    if (attemptCreateOnFailure)
+        try { return repository.create() } catch (e: Throwable) { exception = e }
+
+    throw GameUseCase.GetGameAnswerFailedException(message = exception?.message)
 }
