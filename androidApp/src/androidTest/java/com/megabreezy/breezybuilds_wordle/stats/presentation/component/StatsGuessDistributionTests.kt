471b64ff4461ca17b1ae0a7d47345fa7c204173b
breezybuildsagame <breezy@megabreezy.com>
2023-04-18 07:36:30 -0500

    BBW-102 completed.

--- androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/stats/presentation/component/StatsGuessDistributionTests.kt
+++ androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/stats/presentation/component/StatsGuessDistributionTests.kt
@@ -1,9 +1,17 @@
 package com.megabreezy.breezybuilds_wordle.stats.presentation.component
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
+import com.megabreezy.breezybuilds_wordle.android.core.util.LocalSceneDimensions
 import com.megabreezy.breezybuilds_wordle.android.stats.presentation.component.StatsGuessDistribution
+import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
+import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 import com.megabreezy.breezybuilds_wordle.core.ui.SceneMock
 import org.junit.Rule
 import org.junit.Test
@@ -16,7 +24,7 @@ class StatsGuessDistributionTests
     val composeTestRule = createComposeRule()
 
     @Test
-    fun when_composable_is_displayed__the_expected_TagName_is_applied()
+    fun when_row_component_is_displayed__the_expected_TagName_is_applied()
     {
         // given
         val expectedTagName = "stats_guess_distribution_component_row"
@@ -26,5 +34,46 @@ class StatsGuessDistributionTests
 
         // then
         composeTestRule.onNodeWithContentDescription(expectedTagName).assertExists()
+    }
+
+    @Test
+    fun when_row_component_is_displayed_with_round_and_guessCount__composable_matches_design_requirements()
+    {
+        // given
+        lateinit var expectedRowTextStyle: TextStyle
+        val expectedRoundText = "10"
+        val expectedCorrectGuessCountText = "104"
+
+        // when
+        composeTestRule.setContent()
+        {
+            SceneMock.display()
+            {
+                expectedRowTextStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontFamily = ThemeFonts.roboto,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = dpToSp(dp = LocalSceneDimensions.current.height.times(20 / Scene.idealFrame().height)),
+                    textAlign = TextAlign.Right
+                )
+
+                StatsGuessDistribution.GraphRow.Component(
+                    options = StatsGuessDistribution.GraphRow.ComponentOptions(
+                        round = expectedRoundText,
+                        correctGuessCount = expectedCorrectGuessCountText
+                    )
+                )
+            }
+        }
+        val displayedRow = composeTestRule.onNodeWithContentDescription("${StatsGuessDistribution.GraphRow.TagName.ROW}", useUnmergedTree = true)
+
+        // then
+        listOf(expectedRoundText, expectedCorrectGuessCountText).forEachIndexed()
+        { index, text ->
+            displayedRow.onChildAt(index = index).assertTextEquals(text)
+        }
+        composeTestRule.onNode(
+            SemanticsMatcher.expectValue(StatsGuessDistribution.GraphRow.TextStyleKey, expectedRowTextStyle)
+        ).assertExists()
     }
 }
--- androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/stats/presentation/component/StatsGuessDistribution.kt
+++ androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/stats/presentation/component/StatsGuessDistribution.kt
@@ -1,27 +1,85 @@
 package com.megabreezy.breezybuilds_wordle.android.stats.presentation.component
 
-import android.nfc.Tag
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
+import com.megabreezy.breezybuilds_wordle.android.core.util.LocalSceneDimensions
+import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
+import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 
 object StatsGuessDistribution
 {
     object GraphRow
     {
         @Composable
-        fun Component()
+        fun Component(options: ComponentOptions = ComponentOptions())
         {
+            val textStyle = getRowTextStyle()
+
             Row(
+                horizontalArrangement = Arrangement.spacedBy(
+                    LocalSceneDimensions.current.width.times(5 / Scene.idealFrame().width)
+                ),
                 modifier = Modifier
-                    .semantics { contentDescription = "${TagName.ROW}" }
+                    .semantics()
+                    {
+                        statsGuessDistributionGraphRowTextStyle = textStyle
+                        contentDescription = "${TagName.ROW}"
+                    },
+                verticalAlignment = Alignment.CenterVertically
             )
             {
+                options.round?.let()
+                { roundText ->
+                    Text(
+                        text = roundText,
+                        style = textStyle
+                    )
+                }
 
+                options.correctGuessCount?.let()
+                { correctGuessCount ->
+                    Text(
+                        text = correctGuessCount,
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.error)
+                            .padding(
+                                horizontal = LocalSceneDimensions.current.width.times(5 / Scene.idealFrame().width)
+                            ),
+                        style = textStyle
+                    )
+                }
             }
         }
+
+        @Composable
+        fun getRowTextStyle() = TextStyle(
+            color = MaterialTheme.colorScheme.onSurface,
+            fontFamily = ThemeFonts.roboto,
+            fontWeight = FontWeight.Normal,
+            fontSize = dpToSp(dp = LocalSceneDimensions.current.height.times(20 / Scene.idealFrame().height)),
+            textAlign = TextAlign.Right
+        )
+
+        val TextStyleKey = SemanticsPropertyKey<TextStyle>(name = "TextStyle")
+
+        private var SemanticsPropertyReceiver.statsGuessDistributionGraphRowTextStyle by TextStyleKey
+
+        data class ComponentOptions(val round: String? = null, val correctGuessCount: String? = null)
 
         enum class TagName(private val id: String)
         {
