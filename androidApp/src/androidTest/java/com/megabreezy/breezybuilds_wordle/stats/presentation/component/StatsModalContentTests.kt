b9c8fcac613a2143e0976e39ce84bc60bd47df2d
breezybuildsagame <breezy@megabreezy.com>
2023-04-19 08:38:56 -0500

    BBW-105 Completed.

--- androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/stats/presentation/component/StatsModalContentTests.kt
+++ androidApp/src/androidTest/java/com/megabreezy/breezybuilds_wordle/stats/presentation/component/StatsModalContentTests.kt
@@ -1,11 +1,15 @@
 package com.megabreezy.breezybuilds_wordle.stats.presentation.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.test.SemanticsMatcher
-import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onChildAt
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -16,6 +20,7 @@ import com.megabreezy.breezybuilds_wordle.android.stats.presentation.component.S
 import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
 import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 import com.megabreezy.breezybuilds_wordle.core.ui.SceneMock
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -83,9 +88,86 @@ class StatsModalContentTests
     fun when_component_is_displayed_with_content__the_composable_matches_design_requirements()
     {
         // given
+        val expectedStats = listOf(
+            listOf("100", "Max Streak"),
+            listOf("40", "Win %")
+        )
+        val distributionTitle = "Test Distribution"
+        val expectedButtonLabel = "Play Again"
+        var buttonWasClicked = false
+        lateinit var expectedTitleTextStyle: TextStyle
 
         // when
+        composeTestRule.setContent()
+        {
+            SceneMock.display()
+            {
+                expectedTitleTextStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontFamily = ThemeFonts.roboto,
+                    fontWeight = FontWeight.Black,
+                    fontSize = dpToSp(dp = LocalSceneDimensions.current.height.times(20 / Scene.idealFrame().height))
+                )
+
+                StatsModalContent.Component(
+                    options = StatsModalContent.ComponentOptions(
+                        statsRow = {
+                            Row(
+                                horizontalArrangement = Arrangement.spacedBy(LocalSceneDimensions.current.width.times(5 / Scene.idealFrame().width)),
+                                modifier = Modifier
+                                    .semantics { contentDescription = "${StatsModalContent.TagName.ROW}" },
+                                verticalAlignment = Alignment.Top
+                            )
+                            {
+                                expectedStats.map()
+                                { stat ->
+                                    StatsModalContent.Stat.Component(
+                                        options = StatsModalContent.Stat.ComponentOptions(
+                                            modifier = Modifier.weight(1f),
+                                            headline = stat[0],
+                                            description = stat[1]
+                                        )
+                                    )
+                                }
+                            }
+                        },
+                        guessDistribution = { Text(text = distributionTitle) },
+                        playAgainButton = {
+                            StatsModalContent.PlayAgainButton.Component(
+                                options = StatsModalContent.PlayAgainButton.ComponentOptions(
+                                    label = expectedButtonLabel,
+                                    onClick = { buttonWasClicked = true }
+                                )
+                            )
+                        }
+                    )
+                )
+            }
+        }
+        val displayedModalContent = composeTestRule.onNodeWithContentDescription("${StatsModalContent.TagName.CONTAINER}", useUnmergedTree = true)
+        val displayedStatRow = displayedModalContent.onChildAt(index = 1)
+        val displayedButton = composeTestRule.onNodeWithContentDescription("${StatsModalContent.PlayAgainButton.TagName.BUTTON}", useUnmergedTree = true)
 
         // then
+        displayedModalContent.assertExists()
+        displayedModalContent.onChildAt(index = 0).assertTextEquals("STATISTICS")
+        composeTestRule.onNode(
+            SemanticsMatcher.expectValue(StatsModalContent.TitleTextStyleKey, expectedTitleTextStyle)
+        ).assertExists()
+        displayedModalContent.onChildAt(index = 2).assertTextEquals(distributionTitle)
+        displayedModalContent.onChildAt(index = 3).assertContentDescriptionEquals("${StatsModalContent.PlayAgainButton.TagName.BUTTON}")
+        displayedButton.onChildAt(index = 0).assertTextEquals(expectedButtonLabel)
+        displayedStatRow.assertExists()
+
+        expectedStats.forEachIndexed()
+        { index, stat ->
+            val displayedStat = displayedStatRow.onChildAt(index = index)
+            displayedStat.assertContentDescriptionEquals("${StatsModalContent.Stat.TagName.STAT}")
+            displayedStat.onChildAt(index = 0).assertTextEquals(stat[0])
+            displayedStat.onChildAt(index = 1).assertTextEquals(stat[1])
+        }
+
+        displayedButton.performClick()
+        Assert.assertTrue(buttonWasClicked)
     }
 }
--- androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/stats/presentation/component/StatsModalContent.kt
+++ androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/stats/presentation/component/StatsModalContent.kt
@@ -1,10 +1,17 @@
 package com.megabreezy.breezybuilds_wordle.android.stats.presentation.component
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
@@ -13,6 +20,7 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.megabreezy.breezybuilds_wordle.android.core.ui.Scene
 import com.megabreezy.breezybuilds_wordle.android.core.util.LocalSceneDimensions
 import com.megabreezy.breezybuilds_wordle.android.util.theme.ThemeFonts
@@ -20,6 +28,54 @@ import com.megabreezy.breezybuilds_wordle.android.util.theme.dpToSp
 
 object StatsModalContent
 {
+    @Composable
+    fun Component(options: ComponentOptions = ComponentOptions())
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .semantics { contentDescription = "${TagName.CONTAINER}" }
+        )
+        {
+            val titleTextStyle = getTitleTextStyle()
+            Text(
+                text = "STATISTICS",
+                modifier = Modifier
+                    .semantics { statsModalContentTitleTextStyle = titleTextStyle },
+                style = titleTextStyle
+            )
+
+            options.statsRow?.let { it() }
+
+            options.guessDistribution?.let { it() }
+
+            options.playAgainButton?.let { it() }
+        }
+    }
+
+    val TitleTextStyleKey = SemanticsPropertyKey<TextStyle>("TitleTextStyle")
+
+    private var SemanticsPropertyReceiver.statsModalContentTitleTextStyle by TitleTextStyleKey
+
+    @Composable fun getTitleTextStyle() = TextStyle(
+        color = MaterialTheme.colorScheme.onSurface,
+        fontFamily = ThemeFonts.roboto,
+        fontWeight = FontWeight.Black,
+        fontSize = dpToSp(dp = LocalSceneDimensions.current.height.times(20 / Scene.idealFrame().height))
+    )
+
+    data class ComponentOptions(
+        val statsRow: (@Composable () -> Unit)? = null,
+        var guessDistribution: (@Composable () -> Unit)? = null,
+        var playAgainButton: (@Composable () -> Unit)? = null
+    )
+
+    enum class TagName(private val id: String)
+    {
+        CONTAINER(id = "stats_modal_content_component"), ROW(id = "stats_modal_content_stat_row");
+
+        override fun toString(): String = this.id
+    }
 
     object Stat
     {
@@ -89,6 +145,52 @@ object StatsModalContent
         enum class TagName(private val id: String)
         {
             STAT("stats_modal_content_component_stat");
+
+            override fun toString(): String = this.id
+        }
+    }
+
+    object PlayAgainButton
+    {
+        @Composable
+        fun Component(options: ComponentOptions = ComponentOptions())
+        {
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                ),
+                shape = RoundedCornerShape(
+                    corner = CornerSize(size = LocalSceneDimensions.current.height.times(4 / Scene.idealFrame().height))
+                ),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp),
+                modifier = Modifier
+                    .width(LocalSceneDimensions.current.width.times(275 / Scene.idealFrame().width))
+                    .aspectRatio(ratio = 275 / 50f)
+                    .semantics { contentDescription = "${TagName.BUTTON}" },
+                onClick = options.onClick
+            )
+            {
+                options.label?.let()
+                { labelText ->
+                    Text(
+                        color = MaterialTheme.colorScheme.surface,
+                        text = labelText,
+                        style = TextStyle(
+                            fontFamily = ThemeFonts.roboto,
+                            fontSize = dpToSp(dp = LocalSceneDimensions.current.height.times(14 / Scene.idealFrame().height)),
+                            fontWeight = FontWeight.Black,
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                }
+            }
+        }
+
+        data class ComponentOptions(val label: String? = null, val onClick: () -> Unit = { })
+
+        enum class TagName(private val id: String)
+        {
+            BUTTON(id = "stats_modal_component_play_again_button");
 
             override fun toString(): String = this.id
         }
