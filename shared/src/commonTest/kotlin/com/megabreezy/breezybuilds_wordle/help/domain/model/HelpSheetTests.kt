85ed4d5652caf4866fd5507d522ebc47d0aad11a
breezybuildsagame <breezy@megabreezy.com>
2023-04-26 08:53:58 -0500

    BBW-119 requirements outlined.

---
+++ shared/src/commonTest/kotlin/com/megabreezy/breezybuilds_wordle/help/domain/model/HelpSheetTests.kt
@@ -0,0 +1,96 @@
+package com.megabreezy.breezybuilds_wordle.help.domain.model
+
+import kotlin.test.Test
+
+class HelpSheetTests
+{
+    @Test
+    fun `When Tile initialized with state parameter - the state method returns expected state value`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When Tile initialized with letter char parameter - the letter method returns expected state value`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When Example initialized with tiles list - the tiles method returns expected list`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When Example initialized with description string parameter - the description method returns expected string value`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When Instruction initialized with string parameter - the instruction method returns expected string value`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When HelpSheet initialized with title - the title method returns expected string value`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When HelpSheet initialized with instructions list parameter - the instructions method returns expected list`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When HelpSheet initialized with examples list parameter - the examples method returns expected list`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun `When HelpSheet initialized with footer string parameter - the footer method returns expected string value`()
+    {
+        // given
+
+        // when
+
+        // then
+    }
+}
