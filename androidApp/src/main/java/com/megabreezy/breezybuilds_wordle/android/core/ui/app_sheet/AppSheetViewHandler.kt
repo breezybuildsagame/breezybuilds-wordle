2130957a2a6ee929f7a4c6d41a9ed2c197b868fc
breezybuildsagame <breezy@megabreezy.com>
2023-08-18 12:22:46 -0500

    BBW-113 Completed.

--- androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/core/ui/app_sheet/AppSheetViewHandler.kt
+++ androidApp/src/main/java/com/megabreezy/breezybuilds_wordle/android/core/ui/app_sheet/AppSheetViewHandler.kt
@@ -1,14 +1,21 @@
 package com.megabreezy.breezybuilds_wordle.android.core.ui.app_sheet
 
+import androidx.compose.foundation.background
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.megabreezy.breezybuilds_wordle.android.help.presentation.HelpSheetComposable
 import com.megabreezy.breezybuilds_wordle.core.ui.app_sheet.AppSheetHelper
 import com.megabreezy.breezybuilds_wordle.core.ui.app_sheet.AppSheetRepresentable
 import com.megabreezy.breezybuilds_wordle.core.ui.app_sheet.AppSheetViewHandleable
+import com.megabreezy.breezybuilds_wordle.feature.help.domain.model.HelpSheet
+import kotlinx.coroutines.launch
 
 class AppSheetViewHandler(
     private val appSheet: AppSheetRepresentable = AppSheetHelper().appSheet()
@@ -18,20 +25,63 @@ class AppSheetViewHandler(
 
     fun setUp() { appSheet.setHandler(newHandler = this) }
 
-    override fun onSheetShouldShow(animationDuration: Long)
-    {
-        appSheetIsShowing = true
-    }
+    override fun onSheetShouldShow(animationDuration: Long) { appSheetIsShowing = true }
 
-    override fun onSheetShouldHide(animationDuration: Long)
-    {
-        TODO("Not yet implemented")
-    }
+    override fun onSheetShouldHide(animationDuration: Long) { appSheetIsShowing = false }
 
     @Composable
     fun SheetContent()
     {
-        HelpSheetComposable.Component()
+        val content = appSheet.content()
+        val contentScope = rememberCoroutineScope()
+
+        if (content is HelpSheet)
+        {
+            HelpSheetComposable.Component(
+                options = HelpSheetComposable.ComponentOptions(
+                    title = content.title(),
+                    instructions = content.instructions().map()
+                    {
+                        {
+                            HelpSheetComposable.Instruction.Component(
+                                options = HelpSheetComposable.Instruction.ComponentOptions(
+                                    instruction = it.instruction()
+                                )
+                            )
+                        }
+                    },
+                    examples = content.examples().map()
+                    { example ->
+                        {
+                            HelpSheetComposable.Example.Component(
+                                options = HelpSheetComposable.Example.ComponentOptions(
+                                    tiles = example.tiles().map
+                                    { tile ->
+                                        {
+                                            HelpSheetComposable.Tile.Component(
+                                                options = HelpSheetComposable.Tile.ComponentOptions(
+                                                    state = tile.state(),
+                                                    letter = "$tile"
+                                                )
+                                            )
+                                        }
+                                    },
+                                    description = example.description()
+                                )
+                            )
+                        }
+                    },
+                    footer = content.footer(),
+                    closeButton = {
+                        HelpSheetComposable.CloseButton.Component(
+                            options = HelpSheetComposable.CloseButton.ComponentOptions {
+                                contentScope.launch { content.closeButton().click() }
+                            }
+                        )
+                    }
+                )
+            )
+        }
     }
 }
 
